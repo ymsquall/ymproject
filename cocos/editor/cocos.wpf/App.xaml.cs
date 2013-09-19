a4420eed75405b33fcbd92cebb285600a3fd396b
ymsquall <ymsquare@hotmail.com>
2013-09-19 21:32:34 +0800

    -add Fluent and themes
    Signed-off-by: ymsquall <ymsquare@hotmail.com>

--- cocos/editor/cocos.wpf/App.xaml.cs
+++ cocos/editor/cocos.wpf/App.xaml.cs
@@ -5,6 +5,8 @@ using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using cocos.wpf;
+using Fluent;
 
 namespace cocos.wpf
 {
@@ -13,5 +15,9 @@ namespace cocos.wpf
     /// </summary>
     public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+        }
     }
 }
