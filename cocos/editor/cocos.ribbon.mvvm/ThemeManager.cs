adbfc4938934baccb104cfc59028a979202d0f2e
ymsquall <ymsquare@hotmail.com>
2013-09-24 23:15:10 +0800

    config
    Signed-off-by: ymsquall <ymsquare@hotmail.com>

---
+++ cocos/editor/cocos.ribbon.mvvm/ThemeManager.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cocos.ribbon.mvvm
+{
+    public enum ThemeStyle
+    {
+        Silver,
+        Blue,
+        Black,
+    }
+    public class ThemeManager
+    {
+        public bool ChangeTheme(ThemeStyle style)
+        {
+
+        }
+
+        public bool ChangeTheme(ThemeStyle style)
+        {
+
+        }
+    }
+}
