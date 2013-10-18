38abd56af124c09878f79abb4b75b5ed6b73658a
ymsquall <ymsquare@hotmail.com>
2013-10-18 17:53:48 +0800

    -add c++可跨平台的属性解决方案

--- UnitTest/Property/PropertyTest.h
+++ UnitTest/Property/PropertyTest.h
@@ -14,7 +14,10 @@ private:
 	void setValue3(const int& v);
 
 public:
-	unity::Property<int, PropertyTest, &PropertyTest::setValue1> Value1;
-	unity::PropertyReadOnly<int, PropertyTest> Value2;
-	unity::PropertyWriteOnly<int, PropertyTest, &PropertyTest::setValue3> Value3;
+	PROPERTY_DEFINED(Value1, int, PropertyTest, setValue1);
+	PROPERTY_READONLY_DEFINED(Value2, int, PropertyTest, setValue2);
+	PROPERTY_WRITEONLY_DEFINED(Value3, int, PropertyTest, setValue3);
+	//unity::Property<int, PropertyTest, &PropertyTest::setValue1> Value1;
+	//unity::PropertyReadOnly<int, PropertyTest> Value2;
+	//unity::PropertyWriteOnly<int, PropertyTest, &PropertyTest::setValue3> Value3;
 };
--- cocos/framework/Classes/unity/property.h
+++ cocos/framework/Classes/unity/property.h
@@ -3,38 +3,7 @@
 
 namespace unity
 {
-	//template<class ConT>
-	//class IPropertyContainer
-	//{
-	//public:
-	//	typedef ValueT (ConT::*GetterFunc)();
-	//};
-	//template<class ValueT, class ConT> typedef ValueT (ConT::*GetterFunc)();
-	
-//#define DEFINED_PROPERTY_READ_ONLY(name, type, getter) public: ReadOnlyProperty<type> m#name; \
-//											   public: \
-//											   \
-//											   \
-//#define WRITE_ONLY 2
-//#define READ_WRITE 3
-//	template<class ValueT, typename IPropertyContainer* container, typename char* name>
-//	class PropertyTemplaye
-//	{
-//	public:
-//		PropertyTemplaye(ValueT v)
-//		{
-//			mContainer = container;
-//			mValue = v;
-//			mName = name;
-//		}
-//		
-//	protected:
-//		IPropertyContainer* mContainer;
-//		ValueT mValue;
-//		std::string mName;
-//	};
-
-
+#pragma region 属性基础模板类
 	template<class ValueT, class ConT>
 	class IProperty
 	{
@@ -42,11 +11,14 @@ namespace unity
 		typedef ValueT (ConT::*GetterFunc)();
 		typedef void (ConT::*SetterFunc)(const ValueT& v);
 	};
+#pragma endregion
 
+#pragma region 可读可写属性
 	template<class ValueT, class ConT, typename IProperty<ValueT, ConT>::SetterFunc setter>
 	class Property : public IProperty<ValueT, ConT>
 	{
 	public:
+		friend typename ConT;
 		typedef IProperty<ValueT, ConT> SuperT;
 		typedef Property<ValueT, ConT, setter> ThisT;
 
@@ -71,28 +43,49 @@ namespace unity
 		SuperT::SetterFunc mSetter;
 		ValueT mValue;
 	};
-
-	template<class ValueT, class ConT>
+#define PROPERTY_DEFINED(name, type, container, setter) public: unity::Property<type, container, &container::setter> name;
+#pragma endregion
+	
+#pragma region 只读属性，只能通过容器对象成员函数给予负值，真有用吗？
+	template<class ValueT, class ConT, typename IProperty<ValueT, ConT>::SetterFunc setter>
 	class PropertyReadOnly : public IProperty<ValueT, ConT>
 	{
 	public:
 		friend typename ConT;
 		typedef IProperty<ValueT, ConT> SuperT;
-		typedef PropertyReadOnly<ValueT, ConT> ThisT;
-
+		typedef PropertyReadOnly<ValueT, ConT, setter> ThisT;
+		
+		PropertyReadOnly(ConT* container)
+		{
+			mContainer = container;
+			mSetter = setter;
+		}
 		operator ValueT() const
 		{
 			return mValue;
 		}
-		
+
 	private:
+		void operator = (const ValueT& value)
+		{
+			(mContainer->*mSetter)(value);
+			mValue = value;
+		}
+
+	private:
+		ConT* mContainer;
+		SuperT::SetterFunc mSetter;
 		ValueT mValue;
 	};
-
+#define PROPERTY_READONLY_DEFINED(name, type, container, setter) public: unity::PropertyReadOnly<type, container, &container::setter> name;
+#pragma endregion
+	
+#pragma region 只写属性，估计比只读属性更没用！
 	template<class ValueT, class ConT, typename IProperty<ValueT, ConT>::SetterFunc setter>
 	class PropertyWriteOnly : public IProperty<ValueT, ConT>
 	{
 	public:
+		friend typename ConT;
 		typedef IProperty<ValueT, ConT> SuperT;
 		typedef PropertyWriteOnly<ValueT, ConT, setter> ThisT;
 
@@ -106,10 +99,18 @@ namespace unity
 			(mContainer->*mSetter)(value);
 			mValue = value;
 		}
+		
+	private:
+		operator ValueT() const
+		{
+			return mValue;
+		}
 
 	private:
 		ConT* mContainer;
 		SuperT::SetterFunc mSetter;
 		ValueT mValue;
 	};
+#define PROPERTY_WRITEONLY_DEFINED(name, type, container, setter) public: unity::PropertyWriteOnly<type, container, &container::setter> name;
+#pragma endregion
 }
