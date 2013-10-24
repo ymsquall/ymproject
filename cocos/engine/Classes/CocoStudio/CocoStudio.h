dcf18b1d049b55e21ff2d1584e263ae8e82b6fcd
ymsquall <ymsquare@hotmail.com>
2013-10-24 19:28:57 +0800

    -add cocostudio lib

---
+++ cocos/engine/Classes/CocoStudio/CocoStudio.h
@@ -0,0 +1,30 @@
+#pragma once
+
+#include "CCDeprecated-ext.h"
+#include "Armature/CCArmature.h"
+#include "Armature/CCBone.h"
+#include "Armature/animation/CCArmatureAnimation.h"
+#include "Armature/datas/CCDatas.h"
+#include "Armature/display/CCBatchNode.h"
+#include "Armature/display/CCDecorativeDisplay.h"
+#include "Armature/display/CCDisplayManager.h"
+#include "Armature/display/CCSkin.h"
+#include "Armature/physics/CCColliderDetector.h"
+#include "Armature/utils/CCArmatureDataManager.h"
+#include "Armature/utils/CCDataReaderHelper.h"
+#include "Armature/utils/CCTweenFunction.h"
+#include "Armature/external_tool/sigslot.h"
+
+#include "Components/CCComAttribute.h"
+#include "Components/CCComAudio.h"
+#include "Components/CCComController.h"
+#include "Components/CCComRender.h"
+
+#include "GUI/System/CocosGUI.h"
+
+#include "Json/CSContentJsonDictionary.h"
+#include "Json/DictionaryHelper.h"
+
+#include "Reader/CCSSceneReader.h"
+
+#include "Action/CCActionManager.h"
