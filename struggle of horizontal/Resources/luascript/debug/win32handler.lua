43190bbc54beb4a5c04b71f24f291ea525c8067f
ymsquall <ymsquare@hotmail.com>
2013-11-22 00:34:13 +0800

    -2
    Signed-off-by: ymsquall <ymsquare@hotmail.com>

---
+++ struggle of horizontal/Resources/luascript/debug/win32handler.lua
@@ -0,0 +1,52 @@
+function LUAOnWin32KeyDownMsgProc(wp, lp, hiwp, lowp, hilp, lolp)
+end
+
+function LUAOnWin32KeyUpMsgProc(wp, lp, hiwp, lowp, hilp, lolp)
+end
+
+function LUAOnWin32CharMsgProc(wp, lp, hiwp, lowp, hilp, lolp)
+end
+
+function LUAOnWin32MouseLeftDown(wp, lp, hiwp, lowp, hilp, lolp)
+	return false
+end
+
+function LUAOnWin32MouseLeftUp(wp, lp, hiwp, lowp, hilp, lolp)
+	return false
+end
+
+function LUAOnWin32MouseLeftDBClick(wp, lp, hiwp, lowp, hilp, lolp)
+	return false
+end
+
+function LUAOnWin32MouseRightDown(wp, lp, hiwp, lowp, hilp, lolp)
+	return false
+end
+
+function LUAOnWin32MouseRightUp(wp, lp, hiwp, lowp, hilp, lolp)
+	return false
+end
+
+function LUAOnWin32MouseRightDBClick(wp, lp, hiwp, lowp, hilp, lolp)
+	return false
+end
+
+function LUAOnWin32MouseMiddleDown(wp, lp, hiwp, lowp, hilp, lolp)
+	return false
+end
+
+function LUAOnWin32MouseMiddleUp(wp, lp, hiwp, lowp, hilp, lolp)
+	return false
+end
+
+function LUAOnWin32MouseMiddleDBClick(wp, lp, hiwp, lowp, hilp, lolp)
+	return false
+end
+
+function LUAOnWin32MouseMoveing(wp, lp, hiwp, lowp, hilp, lolp)
+	return false
+end
+
+function LUAOnWin32MouseWheeling(wp, lp, hiwp, lowp, hilp, lolp)
+	return false
+end
