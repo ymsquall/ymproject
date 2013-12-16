21a43cd7c0fb0b62b92136b19ddf8191553e292d
ymsquall <ymsquare@hotmail.com>
2013-12-16 21:24:23 +0800

    -add new animations
    Signed-off-by: ymsquall <ymsquare@hotmail.com>

--- struggle of horizontal/Resources/luascript/models/gamescenemodel_loader.lua
+++ struggle of horizontal/Resources/luascript/models/gamescenemodel_loader.lua
@@ -9,6 +9,8 @@ end
 function LUALoadGameSceneViewResources()
 	local pArmatureDataMgr = ccs.ArmatureDataManager:getInstance()
 	--pArmatureDataMgr:addArmatureFileInfo("heroanim0.png", "heroanim0.plist", "heroanim.json")
-	pArmatureDataMgr:addArmatureFileInfo("girl001.png", "girl001.plist", "girl001.xml")
+	--pArmatureDataMgr:addArmatureFileInfo("girl001.png", "girl001.plist", "girl001.xml")
+	pArmatureDataMgr:addArmatureFileInfo("HeroAnimB0.png", "HeroAnimB0.plist", "HeroAnimB.json")
+	pArmatureDataMgr:addArmatureFileInfo("HeroAnimC0.png", "HeroAnimC0.plist", "HeroAnimC.json")
 end
 
