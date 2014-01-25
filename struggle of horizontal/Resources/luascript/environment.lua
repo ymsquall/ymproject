755a9279bb97c24864eaa112f2f873b4de115b1e
ymsquall <ymsquare_1@163.com>
2014-01-25 22:57:51 +0800

    update 1.0.0.5

--- struggle of horizontal/Resources/luascript/environment.lua
+++ struggle of horizontal/Resources/luascript/environment.lua
@@ -2,8 +2,8 @@ __LUAEnvironmentVariable = {
 	view_width = 1136,
 	view_height = 640,
 	win_title = "soh app",
-	world_gravity = -50.0,		-- 重力
-	move_speed = 380.0,			-- 移动速度
-	jump_power = 25.0,			-- 跳跃力
-	clobber_power = { x = 100.0, y = 15.0 }	-- 被击飞（x方向，y方向）的力
+	world_gravity = -58.0,		-- 重力
+	move_speed = 370.0,			-- 移动速度
+	jump_power = 28.0,			-- 跳跃力
+	clobber_power = { x = 180.0, y = 18.0 }	-- 被击飞（x方向，y方向）的力
 }
